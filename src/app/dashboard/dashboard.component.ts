3cd9cca72765cc14948c6939ceb33dcf1ff4e257
DeVitor <97829726+DeVitorCruz@users.noreply.github.com>
2026-01-21 21:21:01 -0300

    All dashboard features.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,28 +1,20 @@
-import { Component, OnInit  } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
-  dashboardData: any;
+export class DashboardComponent {
 
-  lists: string[] = ['One', 'two', 'three'];
- 
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.authService.getDashboardData().subscribe({
-      next: data => { 
-        this.dashboardData = data; 
-      },
-      error: error => {
-        console.error('Error loading dashboard data', error);
-      }
-    });
-  }
 }
